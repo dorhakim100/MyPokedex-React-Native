a1818313a257cb7203ab600f573aebfafce53ae1
Dor <dorhakim100@gmail.com>
2025-02-02 14:23:00 +0200

    Modify get for caching data

--- app/assets/api/client.js
+++ app/assets/api/client.js
@@ -1,4 +1,5 @@
 import { create } from 'apisauce'
+import cache from '../utility/cache'
 
 const MY_IP = '192.168.1.237'
 const BACKEND_PORT = 3030
@@ -6,5 +7,18 @@ const BACKEND_PORT = 3030
 const apiClient = create({
   baseURL: `http://${MY_IP}:${BACKEND_PORT}/api`,
 })
+
+const get = apiClient.get
+apiClient.get = async (url, params, axiosConfig) => {
+  const response = await get(url, params, axiosConfig)
+
+  if (response.ok) {
+    cache.store(url, response.data)
+    return response
+  }
+
+  const data = await cache.get(url)
+  return data ? { ok: true, data } : response
+}
 
 export default apiClient
